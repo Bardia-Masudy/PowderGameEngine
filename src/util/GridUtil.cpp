192c134b79cc0921a8860c51bc28034ceedb936d
Bardia Masudy <bmasudy@gmail.com>
2026-06-02 11:05:01 -0700

    Implemented mouse dragging using new GridUtils.

--- src/util/GridUtil.cpp
+++ src/util/GridUtil.cpp
@@ -9,16 +9,24 @@ void GridUtil::castRay(float x0, float y0, float x1, float y1, CoordAction& step
     float len = std::sqrt(dx * dx + dy * dy);
     float rdx = dx / len, rdy = dy / len; // unit direction
 
+    if (len < 1e-4f) {
+        int cx = (int) x0, cy = (int) y0;
+        if (!stopFn(cx, cy)) stepFn(cx, cy);
+        return;
+    }
+
     int cx = (int) x0, cy = (int) y0;
     int sx = (rdx >= 0) ? 1 : -1, sy = (rdy >= 0) ? 1 : -1;
 
+    const float inf =  std::numeric_limits<float>::infinity();
     float tDeltaX = std::abs(1.0f / rdx), tDeltaY = std::abs(1.0f / rdy);
-    float tMaxX = std::abs(((rdx != 0 ? cx + sx : cx) - x0) / rdx);
-    float tMaxY = std::abs(((rdy != 0 ? cy + sy : cy) - y0) / rdy);
+    float tMaxX = (rdx != 0) ? std::abs((cx + sx - x0) / rdx) : inf;
+    float tMaxY = (rdy != 0) ? std::abs((cy + sy - y0) / rdy) : inf;
 
     while (true) {
         if (stopFn(cx, cy)) return endFn(cx, cy);
         stepFn(cx, cy);
+        if ((tMaxX < tMaxY ? tMaxX : tMaxY) >= len) return;
         if (tMaxX < tMaxY)  { cx += sx; tMaxX += tDeltaX; }
         else                { cy += sy; tMaxY += tDeltaY; }
     }
